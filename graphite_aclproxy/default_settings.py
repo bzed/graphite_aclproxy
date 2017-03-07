6a18a3a698fe68400a73f5eeb4df555842fab65e
Bernd Zeimetz <bernd@bzed.de>
2017-03-07 12:47:44 +0100

    add some default settings.

--- graphite_aclproxy/default_settings.py
+++ graphite_aclproxy/default_settings.py
@@ -6,8 +6,11 @@ LOG_LEVEL = logging.DEBUG
 LOG_NAME = 'graphite_aclproxy'
 
 REQUESTS_GRAPHITE_URL = 'http://127.0.0.1/'
+REQUESTS_GRAPHITE_AUTH = None
 REQUESTS_CHUNK_SIZE = 1024
 REQUESTS_SSL_VERIFY = True
+
+DYNAMIC_FAVICON_TARGET = 'carbon.agents.*.metricsReceived'
 
 IP_ACL = {
     '127.0.0.1/32' :   [ 'carbon.*.*.*', 'carbon.*.*.*.*', 'carbon.*.*.*.*.*' ]
